2efdf52d603751a58f30cfcfbb1e5100d1fe61cf
Myth <callumshelton2503@gmail.com>
2025-02-23 15:52:56 +1000

    Initial commit

---
+++ src/main/java/org/mythical_studios/sowing/SowingKnitsDataGenerator.java
@@ -0,0 +1,11 @@
+package org.mythical_studios.sowing;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SowingKnitsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
